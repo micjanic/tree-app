3872352e455720659626d3b28e8edbe3e240f031
Michael <micjanic20@gmail.com>
2020-12-10 22:18:02 -0600

    changed to hooks

--- client/src/components/Node.js
+++ client/src/components/Node.js
@@ -1,30 +1,33 @@
-import React, { useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Person from './Person'
 
 function Node({ current, treeData }) {
-    console.log(current)
+    const [childNodes, setChildNodes] = useState([])
 
-    const childNodes = current.children.map((child) =>
-        treeData.find((person) => person.id === child.id)
-    )
+    useEffect(() => {
+        const mappedChildNodes = current.children.map((child) =>
+            treeData.find((person) => person.id === child.id)
+        )
+        setChildNodes(mappedChildNodes)
+    }, [current, treeData])
 
-    const mapPersonFromTreeData = childNodes.map((person) => (
-        <Node
-            key={person.id}
-            previous={current}
-            current={person}
-            treeData={treeData}
-        />
-    ))
-
-    const renderChildren = mapPersonFromTreeData.length > 0 && (
-        <div className="children">{mapPersonFromTreeData}</div>
+    const newChildNodes = childNodes.length > 0 && (
+        <div className="children">
+            {childNodes.map((person) => (
+                <Node
+                    key={person.id}
+                    previous={current}
+                    current={person}
+                    treeData={treeData}
+                />
+            ))}
+        </div>
     )
 
     return (
         <div id={current.id} className="node">
             <Person personData={current} />
-            {renderChildren}
+            {newChildNodes}
         </div>
     )
 }
